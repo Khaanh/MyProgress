34ca1ff0ba485d6565db874534adcf17f0a26498
Khaanh <khanh13nguyen13@gmail.com>
2025-08-10 22:52:27 +0200

    WIP: create counter for pull ups

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-	return <div></div>;
+	const [reps, setReps] = useState(0);
+
+	const handleAddReps = () => {
+		setReps((curVal) => curVal + 1);
+	};
+
+	return (
+		<div>
+			<h1>Pull ups:</h1>
+			<form onSubmit={(e) => e.preventDefault()}>
+				<button onClick={() => handleAddReps()}>+ 1</button>
+				<input type="number" />
+				<p>
+					<span>{reps}</span> reps
+				</p>
+			</form>
+		</div>
+	);
 }
 
 export default App;
